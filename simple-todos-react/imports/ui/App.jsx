783ebdb31a0035d2e88e2b63fdf3a89dea165493
Matheus Ribeiro Batista <batista.762@hotmail.com>
2022-10-28 17:48:44 -0300

    Add Code

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const tasks = [
   {_id: 1, text: 'Primeira Tarefa'},
   {_id: 2, text: 'Segunda Tarefa'},
-  {_id: 3, text: 'Third Task'},
+  {_id: 3, text: 'Terceira Tarefa'},
 ];
  
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1>Seja bem vindo!</h1>
     <Hello/>
     <Info/>
   </div>
