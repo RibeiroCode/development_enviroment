c5f16a33ee98683a16e6db8c03b62c8c4890a2aa
Matheus Ribeiro Batista <batista.762@hotmail.com>
2022-10-28 17:46:37 -0300

    Creating Structure

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 
+const tasks = [
+  {_id: 1, text: ''},
+  {_id: 2, text: 'Second Task'},
+  {_id: 3, text: 'Third Task'},
+];
+ 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
