26600c74e758511866888e49af5063021512bede
Matheus Ribeiro Batista <batista.762@hotmail.com>
2022-11-02 23:12:03 -0300

    Add Code

--- DevelopmentEnviroment-react/imports/ui/App.jsx
+++ DevelopmentEnviroment-react/imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task';
 
+const tasks = [
+  {_id: 1, text: 'Primeira Tarefa'},
+  {_id: 2, text: 'Segunda Tarefa'},
+  {_id: 3, text: 'Terceira Tarefa'},
+];
+ 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>Seja bem vindo(a)!</h1>
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
+
