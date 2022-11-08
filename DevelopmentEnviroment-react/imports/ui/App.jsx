22ea8077cec149edaaa537de1e3e80821626fd03
Matheus Ribeiro Batista <batista.762@hotmail.com>
2022-11-08 15:51:13 -0300

    Add Code

--- DevelopmentEnviroment-react/imports/ui/App.jsx
+++ DevelopmentEnviroment-react/imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
 const tasks = [
   {_id: 1, text: 'Primeira Tarefa'},
